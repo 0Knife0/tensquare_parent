337b8d4a3bd208c557f2102c79232c1ad866d95e
zhuzilong <zhuzilong2013@gmail.com>
2020-04-10 13:54:44 +0800

    文章评论_开发准备

---
+++ tensquare_article/src/main/java/com/tensquare/article/service/CommentService.java
@@ -0,0 +1,12 @@
+package com.tensquare.article.service;
+
+import com.tensquare.article.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentService {
+
+    @Autowired
+    private CommentRepository commentRepository;
+}
