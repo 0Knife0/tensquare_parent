02ec7c9404f363f8f3f923155ab031fa52eefdbb
zhuzilong <zhuzilong2013@gmail.com>
2020-04-10 13:43:34 +0800

    创建评论实体类

---
+++ tensquare_article/src/main/java/com/tensquare/article/pojo/Comment.java
@@ -0,0 +1,19 @@
+package com.tensquare.article.pojo;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+public class Comment implements Serializable {
+    @Id
+    private String _id;
+    private String articleid;   // 评论对应文章id
+    private String content;     // 评论内容
+    private String userid;      // 评论人id
+    private String parentid;    // 评论评论的id
+    private Date publishdate;   // 评论发布时间
+    private Integer thembup;    // 点赞数
+}
