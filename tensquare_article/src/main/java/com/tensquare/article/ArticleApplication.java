f3cc72652ecd8063d77aae813e4d8b7a6a87715d
zhuzilong <zhuzilong2013@gmail.com>
2020-04-13 21:03:02 +0800

    将文章微服务的MapperScan注解移动至Java配置类

--- tensquare_article/src/main/java/com/tensquare/article/ArticleApplication.java
+++ tensquare_article/src/main/java/com/tensquare/article/ArticleApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//配置Mapper包扫描
-@MapperScan("com.tensquare.article.dao")
 public class ArticleApplication {
 
     public static void main(String[] args) {
--- tensquare_article/src/main/java/com/tensquare/article/config/MyBatisPlusConfig.java
+++ tensquare_article/src/main/java/com/tensquare/article/config/MyBatisPlusConfig.java
@@ -1,10 +1,13 @@
 package com.tensquare.article.config;
 
 import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+// 配置Mapper包扫描
+@MapperScan("com.tensquare.article.dao")
 public class MyBatisPlusConfig {
 
     //注入MyBatis分页拦截器
