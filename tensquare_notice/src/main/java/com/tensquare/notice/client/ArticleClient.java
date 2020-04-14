341f6576dcc78b597fdf016839e0a0d3bba50662
zhuzilong <zhuzilong2013@gmail.com>
2020-04-14 17:00:10 +0800

    调用文章微服务根据文章id查询文章名称

---
+++ tensquare_notice/src/main/java/com/tensquare/notice/client/ArticleClient.java
@@ -0,0 +1,15 @@
+package com.tensquare.notice.client;
+
+import com.tensquare.entity.Result;
+import com.tensquare.entity.StatusCode;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient("tensquare-article")
+public interface ArticleClient {
+
+    // GET /article/{articleId} 根据ID查询文章
+    @GetMapping(value = "article/{articleId}")
+    public Result findById(@PathVariable("articleId") String articleId);
+}
